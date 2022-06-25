e18c21510b800add291f17e5fc45fb976b0e1529
vadim.lebediantsev <yfgjktjy1813>
2022-06-25 18:56:09 +0300

    root initializer, creates a root directory

---
+++ src/main/java/notes/project/filesystem/controller/ClusterController.java
@@ -0,0 +1,7 @@
+package notes.project.filesystem.controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClusterController {
+
+}
---
+++ src/main/java/notes/project/filesystem/file/impl/RootInitializerImpl.java
@@ -0,0 +1,36 @@
+package notes.project.filesystem.file.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import notes.project.filesystem.file.RootInitializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class RootInitializerImpl implements RootInitializer {
+    @Value("${root}")
+    private String root;
+
+    private final ApplicationContext applicationContext;
+
+    @Override
+    @EventListener(ApplicationReadyEvent.class)
+    public void initialize() {
+        try {
+            Files.createDirectories(Path.of(root));
+        } catch (IOException e) {
+            log.error("Application stopped with error: {}", e.getMessage());
+            SpringApplication.exit(applicationContext, () -> -1);
+        }
+    }
+}
