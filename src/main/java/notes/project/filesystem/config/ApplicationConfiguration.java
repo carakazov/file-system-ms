b43683ef59622cf4e79a8782df0386ce054c65b6
vadim.lebediantsev <vadim.lebediantsev@bsc-ideas.com>
2022-08-06 19:43:21 +0300

    keycloak

--- src/main/java/notes/project/filesystem/config/ApplicationConfiguration.java
+++ src/main/java/notes/project/filesystem/config/ApplicationConfiguration.java
@@ -8,6 +8,8 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
